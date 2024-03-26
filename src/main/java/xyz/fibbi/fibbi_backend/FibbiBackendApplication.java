14de7ed5922ac17b00c47c0e18ad25881c6c2aff
charlesoller <charlesrello@gmail.com>
2024-03-26 17:13:24 -0400

    initial commit

---
+++ src/main/java/xyz/fibbi/fibbi_backend/FibbiBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.fibbi.fibbi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibbiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibbiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/fibbi/fibbi_backend/FibbiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.fibbi.fibbi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibbiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
